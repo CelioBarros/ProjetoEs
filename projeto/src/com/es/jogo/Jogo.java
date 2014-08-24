a1624581bbb0fd3de00c3f18bef4713bc75461b8
CelioBarros <CelioBarros>
2014-08-23 23:49:57 -0300

    Musica no loop

--- projeto/src/com/es/jogo/Jogo.java
+++ projeto/src/com/es/jogo/Jogo.java
@@ -34,21 +34,23 @@ import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.HorizontalAlign;
 import org.andengine.util.adt.io.in.IInputStreamOpener;
 
+import com.es.R;
 import com.es.banco.DataBase;
 import com.es.projetoes.GameOver;
 
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.CheckBox;
 
 /**
- * (c) 2010 Nicolas Gramlich
- * (c) 2011 Zynga
- *
+ * (c) 2010 Nicolas Gramlich (c) 2011 Zynga
+ * 
  * @author Nicolas Gramlich
  * @since 19:58:39 - 19.07.2010
  */
-public class Jogo extends SimpleBaseGameActivity implements IAccelerationListener{
+public class Jogo extends SimpleBaseGameActivity implements
+		IAccelerationListener {
 	// ===========================================================
 	// Constants
 	// ===========================================================
@@ -59,55 +61,54 @@ public class Jogo extends SimpleBaseGameActivity implements IAccelerationListene
 	// Fields
 	// ===========================================================
 
-	//BackGround Paralelo
+	// BackGround Paralelo
 	private BitmapTextureAtlas mAutoParallaxBackgroundTexture;
 	private ITextureRegion mParallaxLayerBack;
 
-	//Ball
+	// Ball
 	Sprite ballSprite;
 	Context ctx;
 	private ITextureRegion ball;
 	private float xAntigoBola = 0;
 	private float yAntigoBola = 0;
-	//Obstaculo
+	// Obstaculo
 	private BitmapTextureAtlas mBitmapObstaculoTextureAtlas;
 	private TextureRegion mObstaculoTextureRegion;
 	Sprite obstaculoSprite;
-	float teste = 900; 
+	float teste = 900;
 
-	//Buraco
+	// Buraco
 	private BitmapTextureAtlas mBitmapBuracoTextureAtlas;
 	private TextureRegion mBuracoTextureRegion;
 	Sprite buracoSprite;
-	
-	//Bomba
+
+	// Bomba
 	private BitmapTextureAtlas mBitmapBombaTextureAtlas;
 	private TextureRegion mBombaTextureRegion;
 	Sprite bombaSprite;
-	
-	//Moeda
+
+	// Moeda
 	private BitmapTextureAtlas mBitmapMoedaTextureAtlas;
 	private TextureRegion mMoedaTextureRegion;
 	Sprite moedaSprite;
 	Sprite moeda;
 
-
-	//Score	
+	// Score
 	private int score = 0;
 	int countScore = 0;
 	private BitmapFont mBitmapFont;
 	boolean isAlive = true;
 	private Text bitmapText;
 
-	//Listas
+	// Listas
 	private ArrayList<Sprite> listaBlocos = new ArrayList<Sprite>();
 	private ArrayList<Sprite> listaBlocosRemove = new ArrayList<Sprite>();
 	private ArrayList<Sprite> listaFim = new ArrayList<Sprite>();
 	private ArrayList<Sprite> listaFimRemove = new ArrayList<Sprite>();
 	private Music mMusic;
 	DataBase db = new DataBase(this);
-	
-	//private Music myMusic;
+
+	// private Music myMusic;
 
 	// ===========================================================
 	// Constructors
@@ -124,72 +125,91 @@ public class Jogo extends SimpleBaseGameActivity implements IAccelerationListene
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+		EngineOptions engineOptions = new EngineOptions(true,
+				ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(
+						CAMERA_WIDTH, CAMERA_HEIGHT), camera);
 		engineOptions.getAudioOptions().setNeedsMusic(true);
 		return engineOptions;
 	}
 
 	@Override
 	public void onCreateResources() {
-		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/"); 
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		MusicFactory.setAssetBasePath("mfx/");
-		
+
 		try {
-			mMusic = MusicFactory.createMusicFromAsset(getMusicManager(), this, "music.mp3");
+			mMusic = MusicFactory.createMusicFromAsset(getMusicManager(), this,
+					"music.mp3");
+			this.mMusic.setLooping(true);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		
-		
-		this.mBitmapObstaculoTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 32, 32);
-		this.mObstaculoTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBitmapObstaculoTextureAtlas, this, "bloco.png",0,0);
+
+		this.mBitmapObstaculoTextureAtlas = new BitmapTextureAtlas(
+				this.getTextureManager(), 32, 32);
+		this.mObstaculoTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapObstaculoTextureAtlas, this,
+						"bloco.png", 0, 0);
 
 		mBitmapObstaculoTextureAtlas.load();
 
-		this.mAutoParallaxBackgroundTexture = new BitmapTextureAtlas(this.getTextureManager(), 1024, 1024);
+		this.mAutoParallaxBackgroundTexture = new BitmapTextureAtlas(
+				this.getTextureManager(), 1024, 1024);
 
-		this.mParallaxLayerBack = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "background_jogo.png", 0, 188);
+		this.mParallaxLayerBack = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mAutoParallaxBackgroundTexture, this,
+						"background_jogo.png", 0, 188);
 
 		this.mAutoParallaxBackgroundTexture.load();
 
 		try {
 
-			ITexture ballTexture = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
-				@Override
-				public InputStream open() throws IOException {
-					return getAssets().open("ball.png");
-				}
-			});
+			ITexture ballTexture = new BitmapTexture(this.getTextureManager(),
+					new IInputStreamOpener() {
+						@Override
+						public InputStream open() throws IOException {
+							return getAssets().open("ball.png");
+						}
+					});
 
 			ballTexture.load();
 
 			this.ball = TextureRegionFactory.extractFromTexture(ballTexture);
 
-			this.enableAccelerationSensor(this); //Enable Sensor here
+			this.enableAccelerationSensor(this); // Enable Sensor here
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}       
+		}
 
-		this.mBitmapFont = new BitmapFont(this.getTextureManager(), this.getAssets(), "font/BitmapFont.fnt");
+		this.mBitmapFont = new BitmapFont(this.getTextureManager(),
+				this.getAssets(), "font/BitmapFont.fnt");
 		this.mBitmapFont.load();
 
-		//buraco
-		this.mBitmapBuracoTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 70, 69);
-		this.mBuracoTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBitmapBuracoTextureAtlas, this, "buraco.png",0,0);
+		// buraco
+		this.mBitmapBuracoTextureAtlas = new BitmapTextureAtlas(
+				this.getTextureManager(), 70, 69);
+		this.mBuracoTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapBuracoTextureAtlas, this,
+						"buraco.png", 0, 0);
 		mBitmapBuracoTextureAtlas.load();
-		
-		//bomba
-		this.mBitmapBombaTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 32, 32);
-		this.mBombaTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBitmapBombaTextureAtlas, this, "bomba.png",0,0);
+
+		// bomba
+		this.mBitmapBombaTextureAtlas = new BitmapTextureAtlas(
+				this.getTextureManager(), 32, 32);
+		this.mBombaTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapBombaTextureAtlas, this,
+						"bomba.png", 0, 0);
 		mBitmapBombaTextureAtlas.load();
-		
-		//Moeda
-		this.mBitmapMoedaTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 32, 32);
-		this.mMoedaTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBitmapMoedaTextureAtlas, this, "moeda.png",0,0);
+
+		// Moeda
+		this.mBitmapMoedaTextureAtlas = new BitmapTextureAtlas(
+				this.getTextureManager(), 32, 32);
+		this.mMoedaTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapMoedaTextureAtlas, this,
+						"moeda.png", 0, 0);
 		mBitmapMoedaTextureAtlas.load();
 	}
 
@@ -198,104 +218,119 @@ public class Jogo extends SimpleBaseGameActivity implements IAccelerationListene
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
 		final Scene scene = new Scene();
-		//myMusic.play();
-		final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(0, 0, 0, 5);
-		final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
-		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-20f, new Sprite(0, CAMERA_HEIGHT - this.mParallaxLayerBack.getHeight(), this.mParallaxLayerBack, vertexBufferObjectManager)));
+		// myMusic.play();
+		final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(
+				0, 0, 0, 5);
+		final VertexBufferObjectManager vertexBufferObjectManager = this
+				.getVertexBufferObjectManager();
+		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-20f,
+				new Sprite(0, CAMERA_HEIGHT
+						- this.mParallaxLayerBack.getHeight(),
+						this.mParallaxLayerBack, vertexBufferObjectManager)));
 		scene.setBackground(autoParallaxBackground);
 
 		ballSprite = new Sprite(0, 0, this.ball, getVertexBufferObjectManager());
 		final PhysicsHandler physicsHandler = new PhysicsHandler(ballSprite);
 		ballSprite.registerUpdateHandler(physicsHandler);
 
+		// while (true) {
+		// listaBlocos.add(new Sprite(900,100,
+		// mObstaculoTextureRegion,getVertexBufferObjectManager()));
 
-		//while (true) {
-		//		listaBlocos.add(new Sprite(900,100, mObstaculoTextureRegion,getVertexBufferObjectManager()));
+		// }
 
-		//	}
-
-		scene.attachChild(ballSprite);   
-		bitmapText = new Text(0, 0, this.mBitmapFont, "Score:                  ", new TextOptions(HorizontalAlign.CENTER), this.getVertexBufferObjectManager());
+		scene.attachChild(ballSprite);
+		bitmapText = new Text(0, 0, this.mBitmapFont,
+				"Score:                  ", new TextOptions(
+						HorizontalAlign.CENTER),
+				this.getVertexBufferObjectManager());
 		scene.attachChild(bitmapText);
 		/* Create two sprits and add it to the scene. */
 
 		scene.registerUpdateHandler(new IUpdateHandler() {
-			//float testeAux = teste;
+			// float testeAux = teste;
 
 			@Override
-			public void reset() {  
+			public void reset() {
 			}
 
 			@Override
 			public void onUpdate(float pSecondsElapsed) {
-				int numero = (int) (Math.random()*448);
-				if(countScore%101 == 0){
-					listaBlocos.add(new Sprite(900,numero, mObstaculoTextureRegion,getVertexBufferObjectManager()));
+				int numero = (int) (Math.random() * 448);
+				if (countScore % 101 == 0) {
+					listaBlocos.add(new Sprite(900, numero,
+							mObstaculoTextureRegion,
+							getVertexBufferObjectManager()));
 
 				}
-				if(countScore%121 == 0 && countScore!=0){
-					if((int) (Math.random()*2)==0){
-						listaFim.add(new Sprite(900,numero, mBuracoTextureRegion,getVertexBufferObjectManager()));
-					} else{
-						listaFim.add(new Sprite(900,numero, mBombaTextureRegion,getVertexBufferObjectManager()));
+				if (countScore % 121 == 0 && countScore != 0) {
+					if ((int) (Math.random() * 2) == 0) {
+						listaFim.add(new Sprite(900, numero,
+								mBuracoTextureRegion,
+								getVertexBufferObjectManager()));
+					} else {
+						listaFim.add(new Sprite(900, numero,
+								mBombaTextureRegion,
+								getVertexBufferObjectManager()));
 					}
 				}
-				
-				if(countScore%1081 == 0){
-					if(countScore== 0){
-						moeda = new Sprite(1500,numero, mMoedaTextureRegion,getVertexBufferObjectManager());
-					}else{
-						moeda = new Sprite(900,numero, mMoedaTextureRegion,getVertexBufferObjectManager());
+
+				if (countScore % 1081 == 0) {
+					if (countScore == 0) {
+						moeda = new Sprite(1500, numero, mMoedaTextureRegion,
+								getVertexBufferObjectManager());
+					} else {
+						moeda = new Sprite(900, numero, mMoedaTextureRegion,
+								getVertexBufferObjectManager());
 					}
 					scene.attachChild(moeda);
 				}
 
 				for (Sprite bloco : listaBlocos) {
-					//Log.d("teste", bloco.toString());
+					// Log.d("teste", bloco.toString());
 					scene.detachChild(bloco);
 					scene.attachChild(bloco);
 				}
 
 				for (Sprite fim : listaFim) {
-					//Log.d("teste", bloco.toString());
+					// Log.d("teste", bloco.toString());
 					scene.detachChild(fim);
 					scene.attachChild(fim);
 				}
 
-				//Log.d("teste", listaBlocos.size() + "" );
+				// Log.d("teste", listaBlocos.size() + "" );
 				Score();
 				for (Sprite bloco : listaBlocos) {
 
-					float testeAux = bloco.getX()- 2f;
+					float testeAux = bloco.getX() - 2f;
 					bloco.setPosition(testeAux, bloco.getY());
 
 					try {
-						if(isCollides(ballSprite, bloco)){
-							if(ballSprite.getX()==0){
+						if (isCollides(ballSprite, bloco)) {
+							if (ballSprite.getX() == 0) {
 								end();
 								break;
 							}
 							ballSprite.setPosition(xAntigoBola, yAntigoBola);
-							//isAlive= false;
-						}else{
+							// isAlive= false;
+						} else {
 							if (yAntigoBola < ballSprite.getY()) {
-								yAntigoBola = ballSprite.getY()-10;
-							}
-							else if(yAntigoBola > ballSprite.getY()){
-								yAntigoBola = ballSprite.getY()+10;
-							}else{
+								yAntigoBola = ballSprite.getY() - 10;
+							} else if (yAntigoBola > ballSprite.getY()) {
+								yAntigoBola = ballSprite.getY() + 10;
+							} else {
 								yAntigoBola = ballSprite.getY();
 							}
 							if (xAntigoBola != ballSprite.getX()) {
-								xAntigoBola = ballSprite.getX()-6;
+								xAntigoBola = ballSprite.getX() - 6;
 							}
 						}
-						
+
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}  
-					if(bloco.getX()<0){
+					}
+					if (bloco.getX() < 0) {
 						scene.detachChild(bloco);
 						listaBlocosRemove.add(bloco);
 
@@ -305,42 +340,42 @@ public class Jogo extends SimpleBaseGameActivity implements IAccelerationListene
 					listaBlocos.remove(bloco);
 				}
 				listaBlocosRemove.clear();
-				//Log.d("Teste", score + ""  );
+				// Log.d("Teste", score + "" );
 
 				for (Sprite fim : listaFim) {
 
-					float testeAux = fim.getX()- 2f;
+					float testeAux = fim.getX() - 2f;
 					fim.setPosition(testeAux, fim.getY());
 
 					try {
-						if(isCollides(ballSprite,fim) && isAlive){
+						if (isCollides(ballSprite, fim) && isAlive) {
 							end();
 
-							//scene.unregisterUpdateHandler(this);
+							// scene.unregisterUpdateHandler(this);
 							break;
 						}
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}  
-					if(fim.getX()<0){
+					}
+					if (fim.getX() < 0) {
 						scene.detachChild(fim);
 						listaBlocosRemove.add(fim);
 
 					}
 				}
-				
+
 				for (Sprite buraco : listaFimRemove) {
 					listaFim.remove(buraco);
 				}
 				listaFimRemove.clear();
 
-				moeda.setPosition(moeda.getX()-2f, moeda.getY());
-				
-				if(moeda.getX()<0 ){
+				moeda.setPosition(moeda.getX() - 2f, moeda.getY());
+
+				if (moeda.getX() < 0) {
 					scene.detachChild(moeda);
-				} 
-				if(ballSprite.collidesWith(moeda) && isAlive){
+				}
+				if (ballSprite.collidesWith(moeda) && isAlive) {
 					countScore = countScore + 100;
 					scene.detachChild(moeda);
 				}
@@ -348,44 +383,40 @@ public class Jogo extends SimpleBaseGameActivity implements IAccelerationListene
 		});
 		return scene;
 	}
+
 	@Override
 	public void onAccelerationAccuracyChanged(AccelerationData pAccelerationData) {
 		// TODO Auto-generated method stub
 
 	}
-	
-	public void end(){
+
+	public void end() {
 		db.addRanking(score);
 		Intent telaInicial = new Intent(Jogo.this, GameOver.class);
 		startActivity(telaInicial);
 		finish();
-		isAlive= false;
+		isAlive = false;
 	}
 
 	@Override
 	public void onAccelerationChanged(AccelerationData pAccelerationData) {
 
-
 		float positionX = ballSprite.getX() + pAccelerationData.getX();
 		float positionY = ballSprite.getY() + pAccelerationData.getY();
 
-
-		if(positionY > 390) {
+		if (positionY > 390) {
 			positionY = 390;
 		}
-		if(positionY < 0) {
+		if (positionY < 0) {
 			positionY = 0;
 		}
-		if(positionX < 0) {
+		if (positionX < 0) {
 			positionX = 0;
 		}
-		if(positionX > 760) {
+		if (positionX > 760) {
 			positionX = 760;
 		}
-		ballSprite.setPosition(
-				positionX,
-				positionY
-				);
+		ballSprite.setPosition(positionX, positionY);
 
 	}
 
@@ -393,54 +424,51 @@ public class Jogo extends SimpleBaseGameActivity implements IAccelerationListene
 	// Methods
 	// ===========================================================
 
-	public void Score(){
-		if(isAlive){
+	public void Score() {
+		if (isAlive) {
 			this.countScore = countScore + 1;
-			this.score = countScore/100;	
-			bitmapText.setText("Score: " +(int) score);
+			this.score = countScore / 100;
+			bitmapText.setText("Score: " + (int) score);
 		}
 	}
 
-	public void SpawnBlocos(){
+	public void SpawnBlocos() {
 
 	}
-	
-	
+
 	@Override
 	public synchronized void onResumeGame() {
-	  if(mMusic != null && !mMusic.isPlaying()){
-		  
-		  mMusic.play();  
-	    //mMusic.play();
-	  }
-	  super.onResumeGame();
+		if (mMusic != null && !mMusic.isPlaying()) {
+
+			mMusic.play();
+			// mMusic.play();
+		}
+		super.onResumeGame();
 	}
 
-	
 	@Override
 	public synchronized void onPauseGame() {
-	  if(mMusic != null && mMusic.isPlaying()){
-	    mMusic.pause();
-	  }
-	  
-	  super.onPauseGame();
+		if (mMusic != null && mMusic.isPlaying()) {
+			mMusic.pause();
+		}
+
+		super.onPauseGame();
 	}
 
-	public boolean isCollides(Sprite animSprite1 ,Sprite animSprite2) throws Exception{
+	public boolean isCollides(Sprite Sprite1, Sprite Sprite2) throws Exception {
+		float diffX = Math.abs((Sprite1.getX() + Sprite1.getWidth() / 2)
+				- (Sprite2.getX() + Sprite2.getWidth() / 2));
+		float diffY = Math.abs((Sprite1.getY() + Sprite1.getHeight() / 2)
+				- (Sprite2.getY() + Sprite2.getHeight() / 2));
 
+		if (diffX < (Sprite1.getWidth() / 2 + Sprite2.getWidth() / 3)
+				&& diffY < (Sprite1.getHeight() / 2 + Sprite2.getHeight() / 3)) {
 
-		float diffX = Math.abs( (animSprite1.getX() +  animSprite1.getWidth()/2 )- 
-		             (animSprite2.getX() + animSprite2.getWidth()/2 ));
-		float diffY = Math.abs( (animSprite1.getY() +  animSprite1.getHeight()/2 )- 
-		             (animSprite2.getY() + animSprite2.getHeight()/2 ));
-
-		if(diffX < (animSprite1.getWidth()/2 + animSprite2.getWidth()/3) 
-		           && diffY < (animSprite1.getHeight()/2 + animSprite2.getHeight()/3)){
-
-		   return true;
-		}else
-		  return false;
+			return true;
+		} else {
+			return false;
 		}
+	}
 
 	// ===========================================================
 	// Inner and Anonymous Classes
--- projeto/src/com/es/projetoes/Configuracao.java
+++ projeto/src/com/es/projetoes/Configuracao.java
@@ -1,16 +1,12 @@
 package com.es.projetoes;
 
 import com.es.R;
-import com.es.banco.DataBase;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.Checkable;
-import android.widget.TextView;
 
 public class Configuracao extends Activity{
 
